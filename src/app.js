7de5f0fd78d9ec3a83edf113e7156e26ac59a98e
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-04-29 17:03:56 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,9 +7,16 @@ const { maxAttachmentSize } = require('./config')
 
 // Logging Middleware
 app.use((req, res, next) => {
+    const logFilePath = path.join(__dirname, 'requests.log');
     const logEntry = `[${new Date().toISOString()}] ${req.method} ${req.originalUrl} ?${JSON.stringify(req.query)}`;
     console.log(logEntry)
-  
+    
+    fs.appendFile(logFilePath, logEntry + '\n', (err) => {
+        if (err) {
+          console.error('Log failed:', err);
+        }
+      });
+
     next();
   });
 // Initialize Express app
